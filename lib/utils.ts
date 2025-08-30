ba89ffc3a78330b8fda9cc92229bbeef97a0889d
Cosmic AI <support@cosmicjs.com>
2025-08-30 16:20:51 -0700

    Fix deployment error by correcting tailwind-merge package name

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,5 @@
 import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwindcss-merge'
+import { twMerge } from 'tailwind-merge'
 import { Transaction, CategoryBreakdownItem, MonthlyDataItem } from '@/types'
 
 export function cn(...inputs: ClassValue[]) {
